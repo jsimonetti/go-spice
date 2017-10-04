d5cbe3f66df87d524c71140ae3f001b31b86313b
Jeroen Simonetti <jeroen@simonetti.nl>
2017-10-05 00:31:28 +0200

    remove unneeded methods
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- spice/flow.go
+++ spice/flow.go
@@ -1,7 +1,6 @@
 package spice
 
 import (
-	"io"
 	"net"
 	"sync"
 	"time"
@@ -13,88 +12,34 @@ type Flow struct {
 }
 
 func NewFlow(tenant net.Conn, compute net.Conn) *Flow {
-
 	flow := &Flow{
 		tenant:  tenant,
 		compute: compute,
 	}
-
 	return flow
 }
 
-func (f *Flow) proxyData(src net.Conn, dst net.Conn) error {
-	data := make([]byte, 64*1024)
-	pending := 0
-	for {
-		if pending == 0 {
-			var err error
-			pending, err = src.Read(data)
-			if err != nil && err != io.EOF {
-				return err
-			}
-			if pending == 0 {
-				return nil
-			}
-		}
-
-		done, err := dst.Write(data[0:pending])
-		if err != nil {
-			return err
-		}
-		data = data[done:]
-		pending -= done
-	}
-}
-
-func (f *Flow) proxyToCompute() error {
-	err := f.proxyData(f.tenant, f.compute)
-	f.compute.Close()
-	return err
-}
-
-func (f *Flow) proxyToTenant() error {
-	err := f.proxyData(f.compute, f.tenant)
-	f.tenant.Close()
-	return err
-}
-
 func (f *Flow) Proxy() error {
-	Pipe(f.compute, f.tenant)
-	return nil
-
-	go f.proxyToTenant()
-	return f.proxyToCompute()
-}
-
-func writePending(data []byte, dst io.Writer) error {
-	length := len(data)
-	for length > 1 {
-		done, err := dst.Write(data[0:length])
-		if err != nil {
-			return err
-		}
-		data = data[done:]
-		length -= done
-	}
+	f.pipe(f.compute, f.tenant)
 	return nil
 }
 
-func Pipe(src, dst net.Conn) (sent, received int64) {
+func (f *Flow) pipe(src, dst net.Conn) (sent, received int64) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 	go func() {
-		sent = PipeAndClose(src, dst)
+		sent = f.pipeAndClose(src, dst)
 		wg.Done()
 	}()
 	go func() {
-		received = PipeAndClose(dst, src)
+		received = f.pipeAndClose(dst, src)
 		wg.Done()
 	}()
 	wg.Wait()
 	return
 }
 
-func PipeAndClose(src, dst net.Conn) (copied int64) {
+func (f *Flow) pipeAndClose(src, dst net.Conn) (copied int64) {
 	timeout := 10 * time.Second
 	defer dst.Close()
 	buf := make([]byte, 4096)
