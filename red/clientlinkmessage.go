4eec39e348d8d6d18e262f229d6d7836882fd682
Jeroen Simonetti <jeroen@simonetti.nl>
2017-11-24 11:09:38 +0100

    red: add tests to ClientLinkMessage marshal and refactor it
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- red/clientlinkmessage.go
+++ red/clientlinkmessage.go
@@ -1,13 +1,13 @@
 package red
 
-type ClientLinkMessage struct {
-	// Header of the message
+import "encoding/binary"
 
+type ClientLinkMessage struct {
 	// SessionID In   case   of   a   new   session   (i.e.,   channel   type   is
 	// ChannelMain) this field is set to zero, and in response the server will
 	// allocate session id and will send it via the RedLinkReply message. In case of all other
 	// channel types, this field will be equal to the allocated session id.
-	SessionID [4]uint8
+	SessionID uint32
 
 	// ChannelType is one of RED_CHANNEL_?
 	ChannelType ChannelType
@@ -26,8 +26,9 @@ type ClientLinkMessage struct {
 	// member).
 	CapsOffset uint32
 
-	Capabilities1 [4]byte
-	Capabilities2 [4]byte
+	// Capabilities hold the variable length capabilities
+	CommonCapabilities  []uint32
+	ChannelCapabilities []uint32
 }
 
 // NewClientLinkMessage returns an clientLinkMessage
@@ -35,20 +36,63 @@ func NewClientLinkMessage() SpicePacket {
 	return &ClientLinkMessage{}
 }
 
-// MarshalBinary marshals an ArtPollPacket into a byte slice.
+// MarshalBinary marshals a Packet into a byte slice.
 func (p *ClientLinkMessage) MarshalBinary() ([]byte, error) {
 	p.finish()
-	return marshalPacket(p)
+
+	b := make([]byte, int(p.CapsOffset)+4*len(p.CommonCapabilities)+4*len(p.ChannelCapabilities))
+	binary.LittleEndian.PutUint32(b[0:4], uint32(p.SessionID))
+	b[4] = uint8(p.ChannelType)
+	b[5] = p.ChannelID
+	binary.LittleEndian.PutUint32(b[6:10], p.CommonCaps)
+	binary.LittleEndian.PutUint32(b[10:14], p.ChannelCaps)
+	binary.LittleEndian.PutUint32(b[14:18], p.CapsOffset)
+
+	offset := 18
+	for i := 0; i < len(p.CommonCapabilities); i += 4 {
+		binary.LittleEndian.PutUint32(b[i+offset:i+offset+4], p.CommonCapabilities[i])
+	}
+
+	offset = 18 + 4*len(p.CommonCapabilities)
+	for i := 0; i < len(p.ChannelCapabilities); i += 4 {
+		binary.LittleEndian.PutUint32(b[i+offset:i+offset+4], p.ChannelCapabilities[i])
+	}
+
+	return b, nil
 }
 
-// UnmarshalBinary unmarshals the contents of a byte slice into an ArtPollPacket.
+// UnmarshalBinary unmarshals the contents of a byte slice into a Packet.
 func (p *ClientLinkMessage) UnmarshalBinary(b []byte) error {
 	if len(b) < 18 {
 		return errInvalidPacket
 	}
-	full := make([]byte, 26)
-	copy(full, b)
-	return unmarshalPacket(p, full)
+
+	p.SessionID = binary.LittleEndian.Uint32(b[0:4])
+	p.ChannelType = ChannelType(b[4])
+	p.ChannelID = b[5]
+	p.CommonCaps = binary.LittleEndian.Uint32(b[6:10])
+	p.ChannelCaps = binary.LittleEndian.Uint32(b[10:14])
+	p.CapsOffset = binary.LittleEndian.Uint32(b[14:18])
+
+	if len(b) < 18+int(p.CommonCaps)*4+int(p.ChannelCaps)*4 {
+		return errInvalidPacket
+	}
+
+	for i := 18; i < 18+int(p.CommonCaps)*4; i += 4 {
+		if len(b) < i+4 {
+			return errInvalidPacket
+		}
+		p.CommonCapabilities = append(p.CommonCapabilities, binary.LittleEndian.Uint32(b[i:i+4]))
+	}
+
+	for i := 18 + len(p.CommonCapabilities)*4; i < 18+int(p.CommonCaps)*4+int(p.ChannelCaps)*4; i += 4 {
+		if len(b) < i+4 {
+			return errInvalidPacket
+		}
+		p.ChannelCapabilities = append(p.ChannelCapabilities, binary.LittleEndian.Uint32(b[i:i+4]))
+	}
+
+	return p.validate()
 }
 
 // validate is used to validate the Packet.
@@ -58,4 +102,7 @@ func (p *ClientLinkMessage) validate() error {
 
 // finish is used to finish the Packet for sending.
 func (p *ClientLinkMessage) finish() {
+	p.CapsOffset = 18
+	p.CommonCaps = uint32(len(p.CommonCapabilities))
+	p.ChannelCaps = uint32(len(p.ChannelCapabilities))
 }
